4ad5fed4be9c53e2476cefaf567fa287397d39e3
Henrique-Kriguer <henriquekriguer@gmail.com>
2023-03-10 15:02:36 -0300

    commit: Using var to replace variable type

--- java10/src/main/java/org/example/Main.java
+++ java10/src/main/java/org/example/Main.java
@@ -4,14 +4,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        URL url = new URL("https://docs.oracle.com/javase/10/language/");
-        URLConnection urlconnection = url.openConnection();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader((urlconnection.getInputStream())));
+        var url = new URL("https://docs.oracle.com/javase/10/language/");
+        var urlconnection = url.openConnection();
+        var bufferedReader = new BufferedReader(new InputStreamReader((urlconnection.getInputStream())));
 
 
         System.out.println(bufferedReader.lines().collect(Collectors.joining()).replaceAll(">",">\n"));
